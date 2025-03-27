08c68242ff3372ac0cbdcfb5b87f0c4b5201e265
Khang1904 <tnkhang1109@gmail.com>
2025-03-27 20:40:51 +0700

    commit reset

--- template/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAd5M_vkWugvTQ4p3Z4rPKkpcX4JkJ7jXw",
-    authDomain: "coffee-management-92dca.firebaseapp.com",
-    projectId: "coffee-management-92dca",
-    storageBucket: "coffee-management-92dca.firebasestorage.app",
-    messagingSenderId: "348523963586",
-    appId: "1:348523963586:web:721b88f07f56f8e0ce475b",
-    measurementId: "G-KPBRG7MJM8"
-  };
